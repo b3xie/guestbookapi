f21590a47c08c52d9f800bf06e386195017c8601
Gabz@Arch <gabrasan@gmail.com>
2023-05-19 02:13:20 -0300

    allow policies origin again

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 
