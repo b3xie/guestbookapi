cf9ae371c58e3efb329c7ff97ae7cbfda9314e75
Gabz@Arch <gabrasan@gmail.com>
2023-05-19 00:22:10 -0300

    post method

--- api/index.js
+++ api/index.js
@@ -7,12 +7,18 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
+app.use(express.json());
 const testPost = new Post({
   name: "Max",
   post: "bex <3",
 });
 
+app.post("/api/post", (request, response) => {
+  const publicar = new Post({
+    name: request.body.name,
+    post: request.body.post,
+  });
+});
 app.get("/api/posts", (request, response) => {
   Post.find({}).then((posts) => {
     response.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
